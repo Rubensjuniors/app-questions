f929628a73b08002fc489ffff71109cf1d1eb4f0
rubens junio <rubens.marques@hotmart.com>
2025-10-29 12:17:33 -0300

    refactor: remove unused UI components and hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { TooltipProvider } from '@/components/ui/tooltip'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Home from './pages/Home'
 
@@ -6,9 +5,7 @@ const queryClient = new QueryClient()
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Home />
-    </TooltipProvider>
+    <Home />
   </QueryClientProvider>
 )
 
